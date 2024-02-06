9fdc63a76ef73ea50d633857b84876ae7e585896
Alessio <alessio.mazzella@siren.io>
2024-02-06 06:30:44 +0100

    Add API integration to SearchForm component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,13 +2,32 @@ import { useState } from "react";
 
 export default function SearchForm() {
   const [searchText, setSearchText] = useState("");
+  const [responseData, setResponseData] = useState([]);
 
   const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
-  const handleFormInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFormInput = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchText(event.target.value);
+
+    const searchApiAdderss = `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`;
+
+    try {
+      const response = await fetch(searchApiAdderss);
+      if (!response.ok) {
+        throw new Error("Network response error");
+      }
+
+      const data = await response.json();
+      const jobItems = data.jobItems;
+
+      setResponseData(jobItems);
+    } catch {
+      throw new Error("Error fetching data from the server");
+    }
   };
 
   return (
