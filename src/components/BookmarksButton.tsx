0b0a00ae635b4cdfdf24411103ec03c8291480d4
Alessio <alessio.mazzella@siren.io>
2024-02-11 14:24:22 +0100

    Refactor BookmarksButton component to handle outside click event

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,20 +1,41 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function BookmarksButton() {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        !buttonRef.current?.contains(e.target) &&
+        !popoverRef.current?.contains(e.target)
+      ) {
+        setIsPopoverOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   return (
     <section>
       <button
+        ref={buttonRef}
         onClick={() => setIsPopoverOpen((prev) => !prev)}
         className="bookmarks-btn"
       >
         Bookmarks <TriangleDownIcon />
       </button>
 
-      {isPopoverOpen && <BookmarksPopover />}
+      {isPopoverOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
